8bcd5639e3d00e070bec52247f245d944978dd84
totoleheros <thomasbrenard@gmail>
2024-08-29 13:45:50 +0200

    correction list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core'
-import { Cocktail } from './shared/interfaces/cocktail.interface'
-import { CocktailService } from './shared/services/cocktail.service'
+import { Component, OnInit } from '@angular/core';
+import { CocktailService } from './shared/services/cocktail.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-    constructor(private cocktailService: CocktailService) {}
+  constructor(private cocktailService: CocktailService) {}
 
-    ngOnInit(): void {
-        this.cocktailService.getAll().subscribe()
-    }
+  ngOnInit() {
+    this.cocktailService.fetchCocktails().subscribe();
+  }
 }
